c2ede507c5acd457291c477f991e790aee0ce256
fst-gerald <91441878+fst-gerald@users.noreply.github.com>
2024-09-30 20:57:20 +0800

    fix not able to get wifi mac id

--- lib/screens/remaining_time_screen.dart
+++ lib/screens/remaining_time_screen.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:network_info_plus/network_info_plus.dart';
 import '../env/env.dart';
 
 class RemainingTimeScreen extends StatefulWidget {
@@ -16,69 +17,69 @@ class _RemainingTimeScreenState extends State<RemainingTimeScreen> {
   static const platform = MethodChannel('com.example.toto_portal/device_info');
 
   // AUTH CONSTANTS
-  final _username = Env.username;
-  final _password = Env.password;
-  String? _token;
-  Map<String, String>? _cookies;
+  final String username = Env.username;
+  final String password = Env.password;
+  String? authToken;
+  Map<String, String>? cookies;
 
   // APP CONSTANTS
-  String _macAddress = "Unknown";
-  int _remainingSeconds = 0;
-  bool _isLoading = true;
-  Timer? _timer;
+  String macAddress = "Unknown";
+  int remainingSeconds = 0;
+  bool isLoading = true;
+  Timer? countdownTimer;
 
   // URLS AND IDS
-  final String _controllerId = Env.controllerId;
-  final String _siteId = Env.siteId;
-  final String _omadaUrl = Env.omadaUrl;
+  final String controllerId = Env.controllerId;
+  final String siteId = Env.siteId;
+  final String omadaUrl = Env.omadaUrl;
 
   @override
   void initState() {
     super.initState();
-    _getMacAddress();
+    retrieveMacAddress();
   }
 
   @override
   void dispose() {
-    _timer?.cancel();
+    countdownTimer?.cancel();
     super.dispose();
   }
 
-  Future<void> _getMacAddress() async {
+  Future<void> retrieveMacAddress() async {
     try {
       final String result = await platform.invokeMethod('getMacAddress');
       setState(() {
-        _macAddress = result;
+        macAddress = result;
       });
-      await _loginAndFetchToken();
-      await _fetchRemainingTime(_macAddress);
+      await loginAndFetchToken();
+      await fetchRemainingTime(macAddress);
     } on PlatformException catch (e) {
       print("Failed to get MAC address: '${e.message}'.");
     }
   }
 
-  Future<void> _loginAndFetchToken() async {
-    final String loginUrl = '$_omadaUrl/$_controllerId/api/v2/login';
+  Future<void> loginAndFetchToken() async {
+    final String loginUrl = '$omadaUrl/$controllerId/api/v2/login';
 
     try {
       final response = await http.post(
         Uri.parse(loginUrl),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
-          'username': _username,
-          'password': _password,
+          'username': username,
+          'password': password,
         }),
       );
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         setState(() {
-          _token = data['result']['token'];
+          authToken = data['result']['token'];
         });
 
         final cookieHeader = response.headers['set-cookie'];
         if (cookieHeader != null) {
-          _cookies = {'Cookie': cookieHeader}; // Store cookies
+          cookies = {'Cookie': cookieHeader}; // Store cookies
         }
       } else {
         print('Login failed with status: ${response.statusCode}');
@@ -88,68 +89,87 @@ class _RemainingTimeScreenState extends State<RemainingTimeScreen> {
     }
   }
 
-  Future<void> _fetchRemainingTime(String macAddress) async {
+  Future<void> fetchRemainingTime(String mac) async {
+    mac = "02:00:00:00:00:00";
+    if (mac == "02:00:00:00:00:00") {
+      final info = NetworkInfo();
+      final wifiIp = await info.getWifiIP() ?? "";
+      final String clientUrl =
+          '$omadaUrl/$controllerId/api/v2/sites/$siteId/clients?&token=$authToken&searchKey=$wifiIp&sorts.end=desc&filters.active=true&currentPage=1&currentPageSize=1';
+      final response = await http.get(
+        Uri.parse(clientUrl),
+        headers: {
+          'Content-Type': 'application/json',
+          'Csrf-Token': authToken ?? '',
+          if (cookies != null) ...cookies!,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> parsedData = jsonDecode(response.body);
+        mac = parsedData['result']['data'][0]['mac'];
+      }
+    }
+    await fetchClientTime(mac);
+  }
+
+  Future<void> fetchClientTime(String mac) async {
     final String clientUrl =
-        '$_omadaUrl/$_controllerId/api/v2/hotspot/sites/$_siteId/clients?searchKey=$macAddress&sorts.end=desc&token=$_token&currentPage=1&currentPageSize=1';
+        '$omadaUrl/$controllerId/api/v2/hotspot/sites/$siteId/clients?searchKey=$mac&sorts.end=desc&token=$authToken&currentPage=1&currentPageSize=1';
 
     try {
       final response = await http.get(
         Uri.parse(clientUrl),
         headers: {
           'Content-Type': 'application/json',
-          'Csrf-Token': _token ?? '',
-          if (_cookies != null) ..._cookies!,
+          'Csrf-Token': authToken ?? '',
+          if (cookies != null) ...cookies!,
         },
       );
 
       if (response.statusCode == 200) {
         Map<String, dynamic> parsedData = jsonDecode(response.body);
-        // Access the 'end' timestamp
         int endTimestamp = parsedData['result']['data'][0]['end'];
-
-        // Get the current time in milliseconds (Unix timestamp)
         int currentTimestamp = DateTime.now().millisecondsSinceEpoch;
 
-        // Calculate remaining time in milliseconds
         int remainingTimeMillis = endTimestamp - currentTimestamp;
 
         if (remainingTimeMillis < 0) {
           print("Time's up!");
           setState(() {
-            _remainingSeconds = 0;
-            _isLoading = false;
+            remainingSeconds = 0;
+            isLoading = false;
           });
         } else {
-          // Convert remaining time to seconds
-          int remainingSeconds = remainingTimeMillis ~/ 1000;
+          int seconds = remainingTimeMillis ~/ 1000;
           setState(() {
-            _remainingSeconds = remainingSeconds;
-            _isLoading = false;
+            remainingSeconds = seconds;
+            isLoading = false;
           });
-          _startCountdown(); // Start the countdown here after setting the time
+          startCountdown();
         }
       } else {
         print(
             'Failed to load remaining time, status code: ${response.statusCode}');
         setState(() {
-          _isLoading = false;
+          isLoading = false;
         });
       }
     } catch (e) {
       print('Error: $e');
       setState(() {
-        _isLoading = false;
+        isLoading = false;
       });
     }
   }
 
-  void _startCountdown() {
-    _timer?.cancel(); // Cancel any existing timers
+  void startCountdown() {
+    countdownTimer?.cancel();
 
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (_remainingSeconds > 0) {
+    countdownTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (remainingSeconds > 0) {
         setState(() {
-          _remainingSeconds--;
+          remainingSeconds--;
         });
       } else {
         timer.cancel();
@@ -167,14 +187,14 @@ class _RemainingTimeScreenState extends State<RemainingTimeScreen> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: _isLoading
+          child: isLoading
               ? const Text(
                   'Loading...',
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 )
               : Text(
-                  _remainingSeconds > 0
-                      ? _formatTime(_remainingSeconds)
+                  remainingSeconds > 0
+                      ? formatTime(remainingSeconds)
                       : "Time's up!",
                   style: const TextStyle(
                       fontSize: 24, fontWeight: FontWeight.bold),
@@ -182,22 +202,22 @@ class _RemainingTimeScreenState extends State<RemainingTimeScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _fetchRemainingTime(_macAddress),
+        onPressed: () => fetchRemainingTime(macAddress),
         tooltip: 'Refresh',
         child: const Icon(Icons.refresh),
       ),
     );
   }
-}
 
-String _formatTime(int seconds) {
-  final minutes = (seconds / 60).floor();
-  final hours = (minutes / 60).floor();
-  final days = (hours / 24).floor();
+  String formatTime(int seconds) {
+    final minutes = (seconds / 60).floor();
+    final hours = (minutes / 60).floor();
+    final days = (hours / 24).floor();
 
-  final remainingHours = hours % 24;
-  final remainingMinutes = minutes % 60;
-  final remainingSeconds = seconds % 60;
+    final remainingHours = hours % 24;
+    final remainingMinutes = minutes % 60;
+    final remainingSeconds = seconds % 60;
 
-  return '${days}d ${remainingHours}h ${remainingMinutes}m ${remainingSeconds}s';
+    return '${days}d ${remainingHours}h ${remainingMinutes}m ${remainingSeconds}s';
+  }
 }
